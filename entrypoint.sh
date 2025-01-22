ba3851f7c7623ff7ab2303e3b632953717c39ec4
Adrian Pascu <me.adi.hf@gmail.com>
2025-01-22 14:35:27 +0200

    Update to Cirlce CI api v2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -l
 echo "Running QA workflow for PR created on $3 branch"
-project="https://circleci.com/api/v1.1/project/github/$GITHUB_REPOSITORY/tree/$3"
-echo "Running the $2 job for $project"
-response=$(curl -u $1: -d build_parameters[CIRCLE_JOB]=$2 $project)
+response=$(curl -X POST "https://circleci.com/api/v2/project/gh/$GITHUB_REPOSITORY/pipeline" \
+            -H "Circle-Token: $1" \
+            -H "Content-Type: application/json" \
+            -d "{
+              \"branch\": \"$2\",
+              \"parameters\": {$3}
+            }")
 output=$?
 echo $response
 echo ::set-output name=response::$output
