4001798a45e435036d24a07f1d13e9f88bbe524a
Adrian Pascu <me.adi.hf@gmail.com>
2025-01-22 15:44:34 +0200

    Add logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
-echo "Running QA workflow for PR created on $2 branch"
-
+echo "Running QA workflow for PR created for project $2 branch"
+echo "Request url: https://circleci.com/api/v2/project/gh/$GITHUB_REPOSITORY/pipeline"
 echo "Params: {$3}"
 response=$(curl -X POST "https://circleci.com/api/v2/project/gh/$GITHUB_REPOSITORY/pipeline" \
             -H "Circle-Token: $1" \
