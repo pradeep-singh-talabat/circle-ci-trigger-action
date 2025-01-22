29b75f456386e158288d2a8d682f4bb781b259af
Adrian Pascu <me.adi.hf@gmail.com>
2025-01-22 16:02:34 +0200

    Remove logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Running QA workflow for PR created for project $2 branch"
 echo "Request url: https://circleci.com/api/v2/project/gh/$GITHUB_REPOSITORY/pipeline"
 echo "Params: {$3}"
-echo "Key $1"
+
 response=$(curl -X POST "https://circleci.com/api/v2/project/gh/$GITHUB_REPOSITORY/pipeline" \
             -H "Circle-Token: $1" \
             -H "Content-Type: application/json" \
